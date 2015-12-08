dda772f9fcb222edc94f0b5b9026f5a8face487f
Dorian ROLY <droly@e3r10p3.42.fr>
2015-12-08 16:48:41 +0100

    envoi vraiment final

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,14 +6,13 @@
 /*   By: droly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 11:47:35 by droly             #+#    #+#             */
-/*   Updated: 2015/12/08 16:38:02 by droly            ###   ########.fr       */
+/*   Updated: 2015/12/08 16:48:26 by droly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
-
 {
 	size_t i;
 
